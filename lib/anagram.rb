8990ade3f5257077fd735a9df082cc30416fd8b1
Kennedy Musau <kennedy.musauu@gmail.com>
2023-06-28 16:13:15 +0300

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+    def initialize word
+        @word = word
+
+    end
+
+    def match words
+        matched_word = words.select do |match_word| 
+            if match_word.chars.sort == @word.chars.sort
+                match_word
+           end
+         end
+         matched_word
+    end
+end
+
+listen = Anagram.new("listen")
+
+listen.match(%w[enlists google inlet banana])
